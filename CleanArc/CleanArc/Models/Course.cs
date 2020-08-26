6879043886a7da54035f25357748f92c0b1c1de4
solijouana <mr_yaboo1@yahoo.com>
2020-08-26 11:29:04 +0430

    Add Domain Classes

---
+++ CleanArc/CleanArc/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArc.Models
+{
+   public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Descriptions { get; set; }
+        public string ImageUrl { get; set; }    
+    }
+}
