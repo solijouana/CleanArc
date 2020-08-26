33161b0c0669ae71683e1d7917cdccc03a22dd83
solijouana <mr_yaboo1@yahoo.com>
2020-08-26 13:54:05 +0430

    Init Course ViewModel

---
+++ CleanArc/CleanArc.Application/ViewModels/CourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArc.Domain.Models;
+
+namespace CleanArc.Application.ViewModels
+{
+    public class CourseViewModel
+    {
+        public IEnumerable<Course> Courses { get; set; }    
+    }
+}
