bbe247c4c2f2d87a645e4befeda2f0e5b21d5dda
solijouana <mr_yaboo1@yahoo.com>
2020-08-26 11:40:29 +0430

    Add University Context

---
+++ CleanArc/CleanArc.Infra.Data/Context/UniversityAppDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArc.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArc.Infra.Data.Context
+{
+    public class UniversityAppDBContext:DbContext
+    {
+        public UniversityAppDBContext(DbContextOptions<UniversityAppDBContext>options):base(options)
+        {
+            
+        }
+
+        public DbSet<Course> Courses { get; set; }  
+    }
+}
--- CleanArc/CleanArc/Models/Course.cs
+++ CleanArc/CleanArc/Models/Course.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CleanArc.Models
+namespace CleanArc.Domain.Models
 {
    public class Course
     {
